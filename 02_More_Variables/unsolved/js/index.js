06513cbfebd36f8bf7a13d49ebaddd9df6cc3446
smith-sam <74510477+smith-sam@users.noreply.github.com>
2021-05-08 18:38:34 -0500

    Setting up

---
+++ 02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,22 @@
+function addNumbers() {
+    var two = 2;
+    var three = 3;
+
+    var five = two + three;
+
+    console.log(five);
+}
+
+addNumbers();
+
+// Create the stringConcat function here.
+function stringConcat() {
+    var taco = 'Slimy';
+    var cat = 'Seals';
+
+    var tacoCat = taco + ' ' + cat;
+
+    alert(tacoCat);
+}
+
+stringConcat();
